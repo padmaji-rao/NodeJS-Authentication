0650d14d649078794061fda4ab13d660087bf2ed
Padmaji Rao <139029530+padmaji-rao@users.noreply.github.com>
2024-10-14 12:23:51 +0530

    Update AuthServer.js

--- AuthServer.js
+++ AuthServer.js
@@ -2,7 +2,7 @@ const express=require("express")
 const mongoose=require("mongoose")
 const bcrypt=require("bcrypt")
 const jwt=require("jsonwebtoken")
-const Users=require("./authModel")
+const Users=require("./AuthModel")
 
 
 //Middleware Function
